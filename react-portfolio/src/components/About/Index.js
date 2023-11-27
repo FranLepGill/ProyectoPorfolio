4764e885f4918f7501c705c92518d23523509061
Franco Lepratti Gilles <francolepratti@gmail.com>
2023-11-27 02:33:05 -0300

    terminado About y Contact

--- react-portfolio/src/components/About/Index.js
+++ react-portfolio/src/components/About/Index.js
@@ -1,3 +1,89 @@
-const About = () => 'hello'
+import { useEffect, useState } from 'react'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faNode,
+  faReact,
+  faPython,
+  faJava,
+  faFirstOrderAlt,
+  faPhp,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './Index.scss'
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+
+    // Limpia el timeout cuando el componente se desmonta o cuando el efecto se vuelve a ejecutar
+    return () => clearTimeout(timeoutId)
+  }, [])
+
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am a Computer Engineering student seeking a position that enables
+            me to learn and grow while tackling challenges posed by new
+            technologies. I am committed to continuing my studies and
+            contributing my skills to a dynamic work environment.
+          </p>
+          <p align="LEFT">
+            I am a proactive and curious person by nature with a passion for
+            teamwork. I enjoy learning about a variety of topics and
+            technologies.
+          </p>
+          <p>
+            I define myself as a friendly and approachable individual with a
+            versatile range of interests. In addition to my academic pursuits, I
+            enjoy playing the piano and crafting imaginative stories.
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faNode} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faPython} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faJava} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faPhp} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
 
 export default About
